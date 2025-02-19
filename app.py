f1c45acb5f47e62d6e316d592787502de654d58b
Siddeeeq <sadiqaminulawan600@gmail.com>
2025-02-18 18:07:18 -0800

    Added greeting functionality for specific names

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 # Set the page title
-st.title("My First Streamlit Web App")
+st.title("Siddeeeq's Web App")
 
 # Add some text
-st.write("Welcome to my first Streamlit app!")
+st.write("Welcome to my web app!")
 
 # User input
 name = st.text_input("Enter your name:")
@@ -12,3 +12,9 @@ name = st.text_input("Enter your name:")
 # Display dynamic output
 if name:
     st.success(f"Hello, {name}! Glad to have you here.")
+
+if name.lower() == "falamata" or name.lower() == "ameerah":
+    st.write(f"I LOVE YOU SO MUCH MY DEAR {name.upper()}!")
+    st.write(f"How is Egypt?")
+    st.write("")
+    st.write(f"Stay Safe!")
